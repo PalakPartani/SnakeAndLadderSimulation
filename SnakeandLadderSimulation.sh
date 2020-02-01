f5f1c9463d2771a4d9e98e5c8072d4510ef13f64
PalakPartani <partanipalak16@gmail.com>
2020-02-01 17:06:04 +0530

    REFACTORED:constant naming convention

--- SnakeandLadderSimulation.sh
+++ SnakeandLadderSimulation.sh
@@ -1,63 +1,71 @@
 #!/bin/bash -x
 echo "Welcome to Snake and Ladder game ! Good Luck!"
-readonly playerPostion=0
+readonly PLAYERPOSITION=0
 declare -A diceResult
 diceCount=0
 player=1
 
+#Checking the player's turn and change accordingly. 
 checkPlayerTurn()
 {
 	if (( $player == 1 ))
 	then
 		player=2
+		
 	elif (( $player == 2 ))
 	then
 		player=1
 	fi
 }
-rollDice() {
+#Rolling the dice using RANDOM function to generate 6 random values
+rollDice()
+{
 	storeDice=$(($((RANDOM%6))+1))	
-	}
+}
 
-repeatTillWinningPosition(){ 
-	
-	currentPosition=$playerPosition
+#Check untill it reaches the winning position 100
+repeatTillWinningPosition()
+{ 	
+	currentPosition=$PLAYERPOSITION
 	while [[ $currentPosition -le 100 ]]
 	do
 		rollDice
-		currentPosition=${diceResult[$diceCount]}
+		currentPosition=${diceResult[$((diceCount-1))]}
 		((diceCount++))
-			if (( $(($currentPosition+$storeDice)) == 100))
-			then 	
-				diceResult[$diceCount]=100
-				echo "Player $player wins the game!"
-				break
-			elif (( $(($currentPosition+$storeDice)) > 100))
-			then
-				 diceResult[$diceCount]=$currentPosition
-			else
-				checkOption	
-			fi	
-	checkPlayerTurn
+#check until it reaches exact winning position 
+		if (( $(($currentPosition+$storeDice)) == 100))
+		then 	
+			diceResult[$diceCount]=100
+			echo "Player $player wins the game!"
+			break
+		elif (( $(($currentPosition+$storeDice)) > 100))
+		then
+			diceResult[$diceCount]=$currentPosition
+		else
+		   checkOption	
+		fi	
+		checkPlayerTurn
 	done
-	}
-checkOption() {
+}
+#Check whether option is noplay,ladder or snakebite
+checkOption() 
+{
 	option=$(($((RANDOM%3))+1)) 
 	case $option in
-			1)
-				 diceResult[$diceCount]=$currentPosition
-				;;		
-			2)
-				 diceResult[$diceCount]=$(($currentPosition + $storeDice))	
-				;;
-			3)
-				diceResult[$diceCount]=$(($currentPosition - $storeDice))
-				  if (($currentPosition < 0))
-      			then
-        				 currentPosition=$playerPosition
-						diceResult[$diceCount]=$currentPosition
-    				fi
-				;;
+		1)
+     	        diceResult[$diceCount]=$currentPosition
+		;;		
+		2)
+		diceResult[$diceCount]=$(($currentPosition + $storeDice))	
+		;;
+		3)
+		diceResult[$diceCount]=$(($currentPosition - $storeDice))
+		if (($currentPosition < 0))
+      	        then
+		      	currentPosition=$PLAYERPOSITION
+			diceResult[$diceCount]=$currentPosition
+	        fi
+		;;
 	esac
 }
 
