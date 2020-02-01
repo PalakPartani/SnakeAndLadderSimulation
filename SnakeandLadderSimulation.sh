c18ba5b86bd4124ef5dd85cf064d70731e8b08f5
PalakPartani <partanipalak16@gmail.com>
2020-02-01 09:44:14 +0530

    ADDED:function to check options

--- SnakeandLadderSimulation.sh
+++ SnakeandLadderSimulation.sh
@@ -1,5 +1,36 @@
-
 #!/bin/bash -x
 echo "Welcome to Snake and Ladder game ! Good Luck!"
 readonly playerPostion=0
-echo "Random dice value : " $(($((RANDOM%6))+1))
+
+rollDice() {
+	storeDice=$(($((RANDOM%6))+1))	
+}
+
+repeatTillWinningPosition(){ 
+	rollDice
+	while [[ $currentPosition -le 100 ]]
+	do
+		checkOption	
+	done
+	}
+checkOption() {
+	option=$(($((RANDOM%3))+1)) 
+	case $option in
+			1)
+				currentPosition=$currentPosition
+				;;		
+			2)
+				currentPosition=$(($currentPosition + $storeDice))	
+				;;
+			3)
+				currentPosition=$(($currentPosition - $storeDice))
+				  if (($currentPosition < 0))
+      			then
+        				 currentPosition=0
+    				fi
+				;;
+	esac
+}
+
+repeatTillWinningPosition
+
